cf70bd1fad0bc1555bd178daeed001146b980624
Tufazzul1 <tufajjul2017@gmail.com>
2024-08-17 13:38:39 +0600

    created the pagination system

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -28,6 +28,13 @@ const Home = () => {
         },
     });
 
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+    const pages = Array.from({ length: numberOfPages }, (_, i) => i + 1);
+  
+    const handlePaginationButton = (value) => {
+      setCurrentPage(value);
+    };
+  
 
 
     return (
@@ -69,6 +76,68 @@ const Home = () => {
                     )}
                 </div>
             </div>
+            <div className="flex justify-center mt-12">
+                {/* Previous Button */}
+                <button
+                    disabled={currentPage === 1}
+                    onClick={() => handlePaginationButton(currentPage - 1)}
+                    className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-[#3C6D71] hover:text-white"
+                >
+                    <div className="flex items-center -mx-1">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                            />
+                        </svg>
+                        <span className="mx-1">previous</span>
+                    </div>
+                </button>
+                {/* Numbers */}
+                {pages.map((btnNum) => (
+                    <button
+                        onClick={() => handlePaginationButton(btnNum)}
+                        key={btnNum}
+                        className={`hidden ${currentPage === btnNum ? "bg-[#1a373a] text-white" : ""
+                            } px-4 py-2 mx-1 transition-colors duration-300 transform rounded-md sm:inline hover:bg-[#3C6D71] hover:text-white`}
+                    >
+                        {btnNum}
+                    </button>
+                ))}
+                {/* Next Button */}
+                <button
+                    disabled={currentPage === numberOfPages}
+                    onClick={() => handlePaginationButton(currentPage + 1)}
+                    className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-[#3C6D71] disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+                >
+                    <div className="flex items-center -mx-1">
+                        <span className="mx-1">Next</span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M17 8l4 4m0 0l-4 4m4-4H3"
+                            />
+                        </svg>
+                    </div>
+                </button>
+            </div>
+
 
         </div>
     );
